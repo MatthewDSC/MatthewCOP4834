3820eaf0e190659061156c1959d1f15df5d20c1c
imaws <imaws@MATTSLEGIONLAPT>
2022-02-14 19:03:58 -0500

    Add project files.

---
+++ MatthewCOP4834/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatthewCOP4834.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
